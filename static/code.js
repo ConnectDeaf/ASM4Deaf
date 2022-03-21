6f8847e3a68a66a4ff654012662d2738e84027a0
Stavroulla Koumou <stavroullakoumou.a2@gmail.com>
2022-03-21 15:19:10 +0200

    added tags functionality to Add New GIF page

--- static/code.js
+++ static/code.js
@@ -1,6 +1,70 @@
+function create_new_button_badge(new_keyword){
+    let new_badge = document.createElement("button");
+    new_badge.type = "button";
+    new_badge.classList.add("btn");
+    new_badge.classList.add("btn-primary");
+    new_badge.innerText = new_keyword;
+    new_badge.onclick = function(){ //on click, remove self
+        new_badge.remove();
+
+    }
+    return new_badge;
+}
+
+function remove_all_keyword_badges(){
+    let all_badge_buttons = document.querySelectorAll("div#badges button");  
+    all_badge_buttons.forEach(function (item, index){
+        item.remove();
+    });
+
+    let new_keyword_element = document.querySelector("input#new_keyword");
+}
+
+function get_keywords_csv(){
+    let all_badge_buttons = document.querySelectorAll("div#badges button");  
+    let keyword_csv_str = "";  
+    all_badge_buttons.forEach(function (item, index){
+        keyword_csv_str += item.innerText + ((index < (all_badge_buttons.length-1)) ? ',' : '');
+    });
+    return keyword_csv_str;
+}
+
+function prepare_keywords_aux_field(){
+    let keywords = document.querySelector("input#keywords");
+    keywords.value = get_keywords_csv();
+
+    return keywords.value;
+}
+
+//adding badges with the entered keywords
+$("button#add_badge").click(function(e) {
+
+    //get new keyword
+    let new_keyword_element = document.querySelector("input#new_keyword");
+    let new_keyword = new_keyword_element.value;
+    if (new_keyword == "") return;
+    
+    
+    //reset new_keyword input field
+    new_keyword_element.value = "";
+    new_keyword_element.required = false;
+    
+    //create and add a new badge with the keyword
+    let badges_div = document.querySelector("div#badges");
+    badges_div.appendChild(create_new_button_badge(new_keyword));
+});
+
+//submitting the form
 $("form#data").submit(function(e) {
     //don't redirect
     e.preventDefault();
+
+    //prepare the auxiliary input field's value
+    if (prepare_keywords_aux_field() == ""){
+        alert("You canot submit a GIF with no keywords! Please add at least one.");
+        return;
+    }
+
 
     //prepare the data to POST
     var formData = new FormData(this);
@@ -21,6 +85,7 @@ $("form#data").submit(function(e) {
         processData: false
     });
 
-    //clear the form
+    //reset the form and badges area
+    remove_all_keyword_badges();
     this.reset();
 });
