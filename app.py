1a367f93babe0a2220ba9608eb1ec09969b09879
Stavroulla Koumou <stavroullakoumou.a2@gmail.com>
2022-03-22 18:17:27 +0200

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from my_config import FILEPATH_ROOT
+from datauri import DataURI
 import aux_
 
 app = Flask(__name__)
@@ -31,7 +32,7 @@ def login():
     else:
         return render_template("login.html")
 
-@app.route('/new', methods=["POST", "GET"])
+@app.route('/new', methods=["POST", "GET"]) #STATUS: Unfinished
 @app.route('/new/', methods=["POST", "GET"])
 def add_new():
     if request.method == "POST":
@@ -42,7 +43,7 @@ def add_new():
 
             # get GIF type (according to which the rest of the steps into this function 
             #                           will be performed or skipped)
-            ## PENDING: must adapt the input form (add 3 radio buttons)
+            ## PENDING
 
             # create the target save-location path
             ## PENDING
@@ -54,7 +55,7 @@ def add_new():
             #get keywords 
             keywords = request.form["keywords"]
 
-            #save keywords and URL in the database
+            #save keywords and local path in the database
             # PENDING
 
             #return status message
@@ -66,11 +67,7 @@ def add_new():
             return "Failed to add GIF to the database!" 
 
 
-        # FOR search-gif.html PAGE! (to be moved)
-        #create GIF data URI for preview page
-        #gif_uri = DataURI.from_file(filepath)
-        #return preview page
-        #return render_template("search-gif.html", keywords=keywords, gif_uri=gif_uri)
+        
     else:
         return render_template("new-gif.html")
  
@@ -78,9 +75,50 @@ def add_new():
 @app.route('/search', methods=['GET'])
 @app.route('/search/', methods=['GET'])
 def search():
-    pass
+    
+    # form fields presence-check (return the necessary status codes if not correct)
+    ## PENDING (type, keywords, gif file)
+
+    # get GIF type (according to which the rest of the steps into this function 
+    #                           will be performed or skipped)
+    ## PENDING
+    
+    #query the database to find relevant GIFs (get the GIF ids and local paths)
+    ## the table to be queried is determined by the GIF type
+    ## PENDING: will it be a one keyword search or many??
+
+    #retrieve and return the related image Ids??
 
 
+    filepath = FILEPATH_ROOT + "heads\\"
+    keywords = "dummy,keyword,csv,list"
+
+    # FOR search-gif.html PAGE! (to be moved)
+    #create GIF data URI for preview page
+    gif_uri = DataURI.from_file(filepath)
+    #return preview page
+    return render_template("search-gif.html", keywords=keywords, gif_uri=gif_uri)
+
+    return
+
+@app.route("/retrieve", methods=["GET"])
+def retrieve():
+    
+    #(example) http://10.16.20.182:5000/retrieve?type=heads&id=3
+    type = request.args.get('type', None)
+    id = request.args.get('id', None)
+
+
+    #have not yet tried the following 
+    # + I want to learn how to render proper image files into an HTML document
+    #       (not sure if this is posible with my current setup though)
+    
+    #DOWNLOAD_DIRECTORY = FILEPATH_ROOT + "heads\\"
+    #return send_from_directory(DOWNLOAD_DIRECTORY, path, as_attachment=True)
+    ## https://medium.com/analytics-vidhya/receive-or-return-files-flask-api-8389d42b0684
+
+    return f"<h1>{type},{id}</h1>"
+    
 
 
 
